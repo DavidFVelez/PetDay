37c12dc1a0b43e51abbca61d5baf97c6fdf9df36
DavidFVelez <davidf.velez@udea.edu.co>
2023-05-07 01:04:22 -0500

    Implementación de fragmentos y Bottom Navigation

--- app/src/main/java/com/davidvelez/petday/iu/login/ui/login/LoginResult.kt
+++
@@ -1,9 +0,0 @@
-package com.davidvelez.petday.iu.login.ui.login
-
-/**
- * Authentication result : success (user details) or error message.
- */
-data class LoginResult(
-    val success: LoggedInUserView? = null,
-    val error: Int? = null
-)
