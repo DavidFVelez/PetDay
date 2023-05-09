3b5f278838218a70009a57d2e311981a7e831270
JESUS-MORENO <jesu-moreno@hotmail.com>
2023-05-09 14:21:18 -0500

    Actualizacion de fragmentos

--- app/src/main/java/com/davidvelez/petday/iu/register/CarerRegisterFragment.kt
+++ app/src/main/java/com/davidvelez/petday/iu/register/CarerRegisterFragment.kt
@@ -1,60 +1,42 @@
 package com.davidvelez.petday.iu.register
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.fragment.findNavController
 import com.davidvelez.petday.R
+import com.davidvelez.petday.databinding.FragmentCarerRegisterBinding
+import com.davidvelez.petday.iu.bottomnavigation.BottomNavigationActivity
+import com.davidvelez.petday.iu.login.CarerLoginFragmentDirections
+import com.davidvelez.petday.iu.main.MainActivity
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [CarerRegisterFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class CarerRegisterFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+   private lateinit var carerRegisterBinding: FragmentCarerRegisterBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_carer_register, container, false)
+        carerRegisterBinding=FragmentCarerRegisterBinding.inflate(inflater,container,false)
+        val view = carerRegisterBinding.root
+
+
+        carerRegisterBinding.botonRegistrarseFragmentRegistro.setOnClickListener {
+            val registrar = Intent(activity, BottomNavigationActivity::class.java)
+            startActivity(registrar)
+        }
+
+
+
+        return view
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment CarerRegisterFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            CarerRegisterFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
-    }
+
 }
